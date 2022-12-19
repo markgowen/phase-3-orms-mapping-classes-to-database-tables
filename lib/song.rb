e95a3e8c6cec3a57cf2c9159959d27bd81715908
Mark <mark.gowen78@gmail.com>
2022-12-19 16:30:40 -0600

    Lab solution

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,64 @@
 class Song
+  attr_accessor :name, :album, :id
 
-  attr_accessor :name, :album
-
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.album)
+
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    self
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+        SELECT * FROM songs
+        WHERE name = ?
+      SQL
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(name: result[1], album: result[2], id: result[0])
+  end
+
+  def update
+    sql = <<-SQL
+        UPDATE songs
+        SET name = ?, album = ?
+        WHERE id = ?
+      SQL
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
+
+  def self.find(id)
+    sql = <<-SQL
+        SELECT * FROM songs
+        WHERE id = ?
+      SQL
+    result = DB[:conn].execute(sql, id)[0]
+    Song.new(name: result[1], album: result[2], id: result[0])
+  end
 end
